85b7f4b4f1af4954b847df05dcd5f990213bc84b
Stephen Gurnett <stephen.gurnett@gmail.com>
2021-01-26 23:00:10 +0100

    Appointment test

---
+++ appointments/test/Appointment.test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Appointment } from '../src/Appointment'
+
+describe('Appointment', () => {
+    it('renders the customer first name', () => {
+        const customer = { firstNMame: 'Ashley' }
+        const component = <Appointment customer={customer} />
+        const container = document.createElement('div')
+        document.body.appendChild(container)
+
+        ReactDOM.render(component, container)
+
+        expect(document.body.textContent).toMatch('Ashley')
+    })
+
+    it.skip('renders another customer first name', () => {
+        const customer = { firstNMame: 'Jordan' }
+        const component = <Appointment customer={customer} />
+        const container = document.createElement('div')
+        document.body.appendChild(container)
+
+        ReactDOM.render(component, container)
+
+        expect(document.body.textContent).toMatch('Jordan')
+    })
+})
