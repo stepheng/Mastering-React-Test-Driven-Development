f4622d6cddafc963d7b3e514f139fe6a72f628f1
Stephen Gurnett <stephen.gurnett@gmail.com>
2021-01-29 23:02:37 +0100

    Add input tag (readonly)

--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -8,18 +8,29 @@ describe('CustomerForm', () => {
     beforeEach(() => {
         ({ render, container } = createContainer())
     })
-    const form = id => container.querySelector('form[id="${id}"]')
+    const form = id => container.querySelector(`form[id="${id}"]`)
 
     it('renders a form', () => {
         render(<CustomerForm />)
         expect(form('customer')).not.toBeNull()
     })
 
+    const expectToBeInputFieldOfTypeText = formElement => {
+        expect(formElement).not.toBeNull()
+        expect(formElement.tagName).toEqual('INPUT')
+        expect(formElement.type).toEqual('text')
+    }
+
     it('renders the first name field as a text box', () => {
         render(<CustomerForm />)
         const field = form('customer').elements.firstName
-        expect(field).not.toBeNull()
-        expect(field.tagName).toEqual('INPUT')
-        expect(field.type).toEqual('text')
+        expectToBeInputFieldOfTypeText(field)
+    })
+
+    it('includes the existing value for the first name', () => {
+        render(<CustomerForm firstName="Ashley" />)
+        const field = form('customer').elements.firstName
+        expectToBeInputFieldOfTypeText(field)
+        expect(field.value).toEqual('Ashley')
     })
 })
